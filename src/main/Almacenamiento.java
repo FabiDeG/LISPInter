fc3d4f69e85bd0dc70ab2c14a3ac7959da12a039
Fabiola <105880646+FabiDeG@users.noreply.github.com>
2023-03-18 20:50:28 -0600

    Pattern para cada uno de los casos posibles en los que se dividen las operaciones

--- src/main/Almacenamiento.java
+++ src/main/Almacenamiento.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+// A continuacion se leera el archivo y se guardara en una lista que sera utilizada mas adelante
+
 public class Almacenamiento {
     private List<String> instrucciones;
 
@@ -14,7 +16,7 @@ public class Almacenamiento {
         try {
             BufferedReader br = new BufferedReader(new FileReader(archivoLisp));
             String linea;
-            while ((linea = br.readLine()) != null) {
+            while ((linea = br.readLine()) != null) { // Hasta que se lea una linea en blanco, se dejaran de agregar las lineas a la lista
                 instrucciones.add(linea.trim());
             }
             br.close();
--- src/main/Scann.java
+++ src/main/Scann.java
@@ -7,11 +7,21 @@ import java.util.regex.Pattern;
 public class Scann {
     private List<String> operaciones;
     private List<String> palabras;
+    private List<String> vatom;
+    private List<String> vdefun;
+    private List<String> cequal;
+    private List<String> vlist;
+    private List<String> vsetq;
 
     public Scann(List<String> instrucciones) {
         operaciones = new ArrayList<>();
         palabras = new ArrayList<>();
-
+        
+        Pattern patronEqual = Pattern.compile("^\\(set\\b[^()]*[<>][^()]*\\)");
+        Pattern patronList = Pattern.compile("^\\(list\\b.*\\)");
+        Pattern patronAtom = Pattern.compile("^\\(atom\\b.*\\)");
+        Pattern patronDefun = Pattern.compile("^\\(defun\\b.*\\)");
+        Pattern patronSetq = Pattern.compile("^\\(set\\b.*\\)");
         Pattern patronOperacion = Pattern.compile("\\d+\\s*[+\\-*/]\\s*\\d+");
 
         for (String instruccion : instrucciones) {
